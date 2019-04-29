bbcd2030bf8616e8dc62ed63b66ec75e3bce5d9e
satyam88 <nokia123>
2019-04-29 20:03:53 +0530

    codefix

---
+++ src/main/java/com/example/makemyplan/MakemyplanApplication.java
@@ -0,0 +1,13 @@
+package com.example.makemyplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakemyplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakemyplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/makemyplan/MakemyplanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.makemyplan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MakemyplanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
